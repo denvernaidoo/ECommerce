a737273a3bf862a5fd96dfa4c7600c4e045b6344
denvernaidoo <33875237+denvernaidoo@users.noreply.github.com>
2021-10-23 16:25:57 +0200

    added api project

---
+++ ECommerce.API/Controllers/ProductsController.cs
@@ -0,0 +1,27 @@
+﻿using ECommerce.API.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECommerce.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IEnumerable<ApiProduct>> GetAsync()
+        {
+            return new[] { new ApiProduct { Id = Guid.NewGuid(), Description = "fake" } };
+        }
+
+        [HttpPost]
+        public async Task PostAsync()
+        {
+
+        }
+    }
+}
