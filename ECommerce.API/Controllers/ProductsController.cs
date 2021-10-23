bcc8d8663b729e7bd50c6542667246d9b68443b2
denvernaidoo <33875237+denvernaidoo@users.noreply.github.com>
2021-10-23 19:32:02 +0200

    Service fabric actor implementation

--- ECommerce.API/Controllers/ProductsController.cs
+++ ECommerce.API/Controllers/ProductsController.cs
@@ -1,6 +1,10 @@
 ﻿using ECommerce.API.Model;
+using ECommerce.ProductCatalog.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Microsoft.ServiceFabric.Services.Client;
+using Microsoft.ServiceFabric.Services.Remoting.Client;
+using Microsoft.ServiceFabric.Services.Remoting.V2.FabricTransport.Client;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,16 +16,44 @@ namespace ECommerce.API.Controllers
     [Route("api/[controller]")]
     public class ProductsController : ControllerBase
     {
+        private readonly IProductCatalogService _service;
+
+        public ProductsController()
+        {
+            var proxyFactory = new ServiceProxyFactory(c => new FabricTransportServiceRemotingClientFactory());
+
+            _service = proxyFactory.CreateServiceProxy<IProductCatalogService>(
+                new Uri("fabric:/ECommerce/ECommerce.ProductCatalog"),
+                new ServicePartitionKey(0));
+        }
+
         [HttpGet]
         public async Task<IEnumerable<ApiProduct>> GetAsync()
         {
-            return new[] { new ApiProduct { Id = Guid.NewGuid(), Description = "fake" } };
+            IEnumerable<Product> allProducts = await _service.GetAllProductsAsync();
+            return allProducts.Select(p => new ApiProduct 
+            {
+                Id = p.Id,
+                Name = p.Name,
+                Description = p.Description,
+                Price = p.Price,
+                IsAvailable = p.Availability > 0
+            });
         }
 
         [HttpPost]
-        public async Task PostAsync()
+        public async Task PostAsync([FromBody] ApiProduct product)
         {
+            var newProduct = new Product
+            {
+                Id = Guid.NewGuid(),
+                Name = product.Name,
+                Description = product.Description,
+                Price = product.Price,
+                Availability = 100
+            };
 
+            await _service.AddProductAsync(newProduct);
         }
     }
 }
